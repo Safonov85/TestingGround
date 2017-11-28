2652bdf76faf536774d90e70bd6cf35366712f8c
Safonov85 <safonov85@yahoo.com>
2017-11-28 23:45:38 +0100

    First Commit

---
+++ TestingGround/MakeAList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingGround
+{
+	public class MakeAList
+	{
+		public List<string> names = new List<string>();
+
+		public MakeAList()
+		{
+			names.Add("Matthew");
+			names.Add("Mark");
+			names.Add("Luke");
+			names.Add("John");
+			names.Add("Paul");
+			names.Add("Judas");
+			names.Add("Pilatus");
+			names.Add("Herod");
+		}
+	}
+}
